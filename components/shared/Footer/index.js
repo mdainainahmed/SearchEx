2d89681187ec0fd71425ad07e54922aba6a0923a
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-10-25 20:27:37 +0530

    Update index.js

--- components/shared/Footer/index.js
+++ components/shared/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
             <div className={styles.footer__text}>
                 {/* <p className={styles.location} ><BsGeoAlt /> {location} </p> */}
                 <GhStarBtn />
-                <p> <span className={styles.logo}>SearchEx</span> is a Free and Open Source project. <br /> You can view its source code on <a href="https://github.com/devxprite/searchex" target="_blank">Github</a> </p>
+                <p> <span className={styles.logo}>SearchEx</span> is a Free and Open Source project. <br /> You can view its source code on <a href="https://github.com/mdainainahmed/SearchEx" target="_blank">Github</a> </p>
                 <ThemeBtn />
             </div>
         </footer>
